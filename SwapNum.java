295d77b391e1af84bf1b529b2c6e077557881348
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-24 23:33:35 +0530

    Add files via upload

---
+++ SwapNum.java
@@ -0,0 +1,13 @@
+public class SwapNum
+    {
+      public static void main(String[] args)
+        {  
+          int num1= 10;
+          int num2= 20;
+	  System.out.println("Num1 and Num2 before swap   =  " +num1+ "  ,  " +num2);	
+          num1=num1+num2;
+          num2=num1-num2;
+          num1=num1-num2;
+          System.out.println("Num1 and Num2 afetr swap   =  " +num1+ "  ,  " +num2);
+        }
+    }
