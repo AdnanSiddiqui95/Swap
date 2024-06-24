1d689c70a73de281a2232049827c5912f978f9d3
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-24 23:35:42 +0530

    Update SwapNum.java

--- SwapNum.java
+++ SwapNum.java
@@ -2,6 +2,7 @@ public class SwapNum
     {
       public static void main(String[] args)
         {  
+	// Swapping of two variables without third variable (possible with Int,float datatype)
           int num1= 10;
           int num2= 20;
 	  System.out.println("Num1 and Num2 before swap   =  " +num1+ "  ,  " +num2);	
